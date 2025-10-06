021310ae51d177834a4c4ee9dfecad4e1e56bc31
Harshit Upadhyay <harshitu149@gmail.com>
2025-10-06 19:01:12 +0530

    updated frontend

--- apps/drawapp-frontend/app/dashboard/page.tsx
+++ apps/drawapp-frontend/app/dashboard/page.tsx
@@ -2,7 +2,5 @@ import CreateRoom from "../components/createRoom";
 import HeroSection from "../components/herosection";
 
 export default function Dashboard() {
-  return <div className="pt-16">
-       
-  </div>
+    
 }
